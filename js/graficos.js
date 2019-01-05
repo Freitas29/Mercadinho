401c555e53afc536e4d24ca25a608a9b02e266c2
Freitas29 <arao.freitas@hotmail.com>
2019-01-04 22:53:44 -0200

    cores diferentes

--- js/graficos.js
+++ js/graficos.js
@@ -6,13 +6,13 @@ function gerarGrafico() {
             //Separando os JSON para dois arrays 
             var sep = retorno.split("]");
             //Pegando os arrays e add a chave que foi retirada
-            nomC = sep[0]+"]";
-            qtdC = sep[1]+"]";
-            
+            nomC = sep[0] + "]";
+            qtdC = sep[1] + "]";
+
             //Transformando o json em objeto
             var nomeP = JSON.parse(nomC);
             var qtdP = JSON.parse(qtdC);
-            
+          
             //Transformando em array
             const nomeProduto = Object.keys(nomeP).map(i => nomeP[i]);
             const qtdProduto = Object.keys(qtdP).map(i => qtdP[i]);
@@ -22,14 +22,33 @@ function gerarGrafico() {
             var resultadoVendas = qtdP;
             //Instânciando o objeto e escolhendo o tipo
             let vendas = new Chart(grafico, {
-                type: 'bar',
+                type: 'horizontalBar',
+                
                 data: {
                     labels: produtos,
 
                     datasets: [
+                        
                         {
                             label: 'Vendas',
-                            data: resultadoVendas
+                            data: resultadoVendas,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+
                         }
                     ]
                 },
@@ -59,3 +78,16 @@ function gerarGrafico() {
 }
 
 gerarGrafico();
+
+function gera_cor(){
+    var hexadecimais = '0123456789ABCDEF';
+    var cor = '#';
+  
+    // Pega um número aleatório no array acima
+    for (var i = 0; i < 1000; i++ ) {
+    //E concatena à variável cor
+        cor += hexadecimais[Math.floor(Math.random() * 16)];
+    }
+    return cor;
+}
+
